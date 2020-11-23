220d443c65ce8e766e818d499f1dfe9c56d8d0f3
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2020-11-23 11:13:58 -0300

    feat: adding api request pokemon

--- src/pages/Pokemon/index.jsx
+++ src/pages/Pokemon/index.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+import Characters from "../../components/characters";
+
 const Pokemon = () => {
-  return <div>aloooooo pokemon</div>;
+  const [characterList, setCharacterList] = useState([]);
+  const [page, setPage] = useState(20);
+
+  const getData = async () => {
+    const response = await Axios.get(
+      `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=20`
+    );
+    console.log(response.data.results);
+    setCharacterList([...characterList, ...response.data.results]);
+  };
+
+  const changePage = () => {
+    setPage(page + 20);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+  return (
+    <div>
+      <Characters characterList={characterList} />
+      <button onClick={changePage}>Pŕoxima página</button>
+    </div>
+  );
 };
 
 export default Pokemon;
